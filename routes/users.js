c78209d6e77583f3415d8de1b029ac1716ba70c7
tocology <hwangjun7777@gmail.com>
2016-04-09 12:36:48 +0900

    feat: Modify

--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,9 @@
 var express = require('express');
 var router = express.Router();
 const db = require('../db')
-const fetch = require('isomorphic-fetch')
 
 var sign = 0;
+var userArr = [];
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -22,34 +22,60 @@ router.post('/', (req, res, next) => {
         address: req.body.address
     }
 
-    let newUser = new db.userModel({
-        name: profile.name,
-        phoneNum: profile.phoneNum,
-        company: profile.company,
-        email: profile.email,
-        department: profile.department,
-        address: profile.address,
+    userArr.push(profile)
+
+    res.render('trade', {
+        name: profile.name
     })
 
-    newUser.save(error => {
-        if(error) {
-            console.log(err)
-        } else {
-            res.render('trade', {
-                name: profile.name
-            })
-        }
-    })
+    //let newUser = new db.userModel({
+    //    name: profile.name,
+    //    phoneNum: profile.phoneNum,
+    //    company: profile.company,
+    //    email: profile.email,
+    //    department: profile.department,
+    //    address: profile.address,
+    //})
+    //
+    //newUser.save(error => {
+    //    if(error) {
+    //        console.log(err)
+    //    } else {
+    //        res.render('trade', {
+    //            name: profile.name
+    //        })
+    //    }
+    //})
 })
 
 router.get('/insertSignal', (req, res, next) => {
     sign = 1;
+    console.log(sign)
     res.send("Sign Inserted")
 })
 
 router.post('/insertSignal', (req, res, next) => {
     sign = req.body.sign
     res.send("Sign Inserted")
+})
+
+router.post('/checkSignal', (req, res, next) => {
+    let name = req.body.name
+
+    console.log(userArr)
+
+    if(sign == 1){
+        console.log("sign checked")
+        for(let i=0; i<userArr.length; ++i){
+            console.log(userArr[i])
+            console.log(userArr[i].name !== name)
+            if(userArr[i].name !== name){
+                console.log("in "+ userArr[i])
+                res.json(userArr[i])
+                break
+            }
+        }
+    }
 })
 
 module.exports = router;
